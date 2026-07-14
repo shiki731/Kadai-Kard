6ca6ac6ec127d7f33b09ea66f7005e16cd8805c4
aaa <ktc25A31A0022@edu.kyoto-tech.ac.jp>
2026-07-14 18:00:23 +0900

    aaa

---
+++ Assets/Scripts/KardInfoGet.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class KardInfoGet : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, 1.0f);
+        }
+    }
+}
---
+++ Assets/Scripts/KardScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class KardScript : MonoBehaviour
+{
+    public int KardNum = 0;
+    public int InfoIntNum = 0;
+    public int InfoStrNum = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
