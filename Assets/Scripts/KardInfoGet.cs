fd6416ee3b579a44608e9065fe562bae8554df03
shiki <ktc25A31A0022@edu.kyoto-tech.ac.jp>
2026-07-21 04:30:43 +0900

    aaa

--- Assets/Scripts/KardInfoGet.cs
+++ Assets/Scripts/KardInfoGet.cs
@@ -1,8 +1,14 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class KardInfoGet : MonoBehaviour
 {
+    public static int MyKardNum;
+    
+
+    private KardScript _kardScript;
+    [SerializeField] private KardScript Enemy;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,13 +18,69 @@ public class KardInfoGet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, 1.0f);
-            if(hit.collider.tag == "Card")
+            Vector2 mousePos = Mouse.current.position.ReadValue();
+            Vector2 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            Collider2D hit = Physics2D.OverlapPoint(worldPos);
+            if (hit != null)
             {
-                Debug.Log("aaa");
+                if (hit.CompareTag("Card"))
+                {
+                    
+                    _kardScript = hit.gameObject.GetComponent<KardScript>();
+                    MyKardNum = Mathf.Clamp(_kardScript.KardNum, 1, 13);
+                    Debug.Log(_kardScript.KardNum);
+
+                }
+                else
+                {
+                    Debug.Log("bbb");
+                }
             }
+
+            if (MyKardNum == Enemy.KardNum)
+            {
+                _kardScript.CardDrow();
+            }
+            else if (MyKardNum + 1 == Enemy.KardNum)
+            {
+                Enemy.KardNum--;
+                _kardScript.CardDrow();
+            }
+            else if (MyKardNum - 1 == Enemy.KardNum) // —á:MyCard 3 , Enemy 2
+            {
+                Enemy.KardNum++;
+                _kardScript.CardDrow();
+            }
+            else if (MyKardNum == 1 && Enemy.KardNum == 13)
+            {
+                Enemy.KardNum = 1;
+                _kardScript.CardDrow();
+            }
+            else if (MyKardNum == 13 && Enemy.KardNum == 1)
+            {
+                Enemy.KardNum = 13;
+                _kardScript.CardDrow();
+            }
+            MyKardNum = 0;
+        }
+
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            if(KardScript.deck[Enemy.KardNum - 1] <= 3)
+            {
+                KardScript.deck[Enemy.KardNum - 1]++;
+                Enemy.CardDrow();
+            }
+            else
+            {
+                Enemy.CardDrow();
+            }
+
+                
         }
     }
 }
--- Assets/Scripts/KardScript.cs
+++ Assets/Scripts/KardScript.cs
@@ -1,19 +1,54 @@
+using TMPro;
 using UnityEngine;
 
 public class KardScript : MonoBehaviour
 {
+    public static int[] deck;
+
     public int KardNum = 0;
-    public int InfoIntNum = 0;
-    public int InfoStrNum = 0;
+    public TextMeshProUGUI KardNumIllust;
+
+    private int CheckNum;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        deck = new int[13];
+        for(int i = 0;i < 13; i++)
+        {
+            deck[i] = 4;
+        }
+
+
+        CardDrow();
     }
 
     // Update is called once per frame
     void Update()
     {
+        KardNumIllust.text = KardNum.ToString();
+    }
+
+    public void CardDrow()
+    {
+        while (true)
+        {
+            //Debug.Log("AA");
+            CheckNum = Random.Range(1, 14);
+            if (deck[CheckNum - 1] > 0)
+            {
+                KardNum = CheckNum;
+                deck[CheckNum - 1]--;
+
+                // Debug.Log(KardNum);
+                break;
+            }
+            else
+            {
+                
+                Debug.Log("èdï°");
+                return;
+            }
+        }
         
     }
 }
